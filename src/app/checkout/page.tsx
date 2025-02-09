8c9859330bc9b8064c3a4d500a130926fda44ddb
joelikenga <joelikenga@gmail.com>
2025-02-09 17:58:36 +0200

    checkout desktop  fixed

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import { Body } from "./component/body";
+
+const Checkout = () => {
+    return(
+        <main className="">
+            <Body/>
+        </main>
+    )
+}
+
+export default Checkout;
